018d010c80bb594c83e5728d14310b708f7c3ca0
zhu0Li <522192251@qq.com>
2023-06-08 00:53:49 +0800

    搭建后端环境，准备全局异常处理，自定义异常

---
+++ common/service-util/src/main/java/com/atguigu/ssyx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.atguigu.ssyx.common.exception;
+
+import com.atguigu.ssyx.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author ：zhuo
+ * @description：全局异常处理
+ * @date ：2023/6/8 0:44
+ */
+
+//*AOP
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail(null);
+    }
+
+    //自定义异常处理
+    @ExceptionHandler(SsyxException.class)
+    public Result error(SsyxException e){
+        return Result.fail(null);
+    }
+}
