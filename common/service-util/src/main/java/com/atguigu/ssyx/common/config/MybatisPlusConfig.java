c0b0172f9e782db4eb7af5ff4b3bca92285ca0b3
zhu0Li <522192251@qq.com>
2023-06-08 00:16:42 +0800

    搭建后端环境，准备mybatisplus分页插件，准备统一返回结果类

---
+++ common/service-util/src/main/java/com/atguigu/ssyx/common/config/MybatisPlusConfig.java
@@ -0,0 +1,37 @@
+package com.atguigu.ssyx.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：zhuo
+ * @description：mybatisplus 分页插件配置
+ * @date ：2023/6/7 17:27
+ */
+@Configuration
+@MapperScan("com.atguigu.ssyx.*.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor optimisticLockerInnerInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //向Mybatis过滤器链中添加分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+}
